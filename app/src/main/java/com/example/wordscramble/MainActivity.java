4f8e08b34752bf3a3795618721d89913baf7412f
Cadence <cadence242@roadrunner.com>
2022-02-01 08:52:57 -0800

    Initial commit

---
+++ app/src/main/java/com/example/wordscramble/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.wordscramble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View v)
+    {
+        Log.i("info", "this is a test");
+    }
+}
