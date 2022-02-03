68c733722146b7707856be0692a12d3e7410217d
Cadence <cadence242@roadrunner.com>
2022-02-03 08:26:25 -0800

    able to switch from home screen to game screen

--- app/src/main/java/com/example/wordscramble/MainActivity.java
+++ app/src/main/java/com/example/wordscramble/MainActivity.java
@@ -2,21 +2,37 @@ package com.example.wordscramble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (Button)findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v)
+            {
+                Intent test = new Intent(MainActivity.this, gameScreen.class);
+
+                startActivity(test);
+            }
+        });
     }
 
-    public void click(View v)
-    {
-        Log.i("info", "this is a test");
-    }
+//    public void click(View v)
+//    {
+//        Log.i("info", "this is a test");
+//    }
 }
