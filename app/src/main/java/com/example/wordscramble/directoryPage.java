4d2ecb07a20c5658e5413338fb171379d778c5a9
Cadence <cadence242@roadrunner.com>
2022-02-04 08:56:29 -0800

    set button to go to gameScreen

--- app/src/main/java/com/example/wordscramble/directoryPage.java
+++ app/src/main/java/com/example/wordscramble/directoryPage.java
@@ -2,13 +2,33 @@ package com.example.wordscramble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class directoryPage extends AppCompatActivity {
+
+    Button unscramble;
+    Button wordBegin;
+    Button wordEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directory_page);
+
+        unscramble = (Button)findViewById(R.id.wordUnscramble);
+
+        unscramble.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v)
+            {
+                Intent directory = new Intent(directoryPage.this, gameScreen.class);
+                Log.i("info", "switch to directory page");
+                startActivity(directory);
+            }
+        });
     }
 }
