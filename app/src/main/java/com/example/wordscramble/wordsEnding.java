d8d5fbed8d4678d17748e8099c446ff54082f7cc
Cadence <cadence242@roadrunner.com>
2022-02-07 08:24:34 -0800

    added button to go back to directoryPage from wordsEnding

--- app/src/main/java/com/example/wordscramble/wordsEnding.java
+++ app/src/main/java/com/example/wordscramble/wordsEnding.java
@@ -2,13 +2,31 @@ package com.example.wordscramble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class wordsEnding extends AppCompatActivity {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_words_ending);
+
+        back = (Button)findViewById(R.id.backButton3);
+
+        back.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v)
+            {
+                Intent directory = new Intent(wordsEnding.this, directoryPage.class);
+                Log.i("info", "switch to directory page");
+                startActivity(directory);
+            }
+        });
     }
 }
